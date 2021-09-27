e67ac410820c8279dd54a56fa1f6127d3d2c59c5
Bian Li <lib14@Bians-MacBook.local>
2021-09-27 16:45:31 -0500

    Add more information to the About page.

--- apps/about.py
+++ apps/about.py
@@ -30,7 +30,37 @@ about_layout = html.Div(
             '''
             If you find COSMIS helpful in your research, please consider citing the COSMIS paper.
             '''
-        )
+        ),
+        html.Br(),
+        html.Span(
+            '''
+            Li, B., Roden, D.M., and Capra, J.A. (2021). The 3D spatial constraint on 6.1 million 
+            amino acid sites in the human proteome. 
+            bioRxiv. doi: https://doi.org/10.1101/2021.09.15.460390
+            '''
+        ),
+        html.Br(),
+        html.Br(),
+        html.H4(
+            'Source Code',
+        ),
+        html.Span(
+            '''
+            Source code for the COSMIS framework and all scripts for reproducing figures reported
+            in the paper are available at https://github.com/CapraLab.
+            '''
+        ),
+        html.Br(),
+        html.Br(),
+        html.H4(
+            'Contact',
+        ),
+        html.Span(
+            '''
+            This project is maintained by the Capra Lab at Vanderbilt University. For contact 
+            information and information on COSMIS and other tools, please visit us at www.capra.org.
+            '''
+        ),
     ],
     style={'padding': 10}
 ),
